b69cf629ff389c578d866a92215cafaec110d020
Mathias Lorenzen <mathias.lorenzen@live.com>
2018-02-12 17:59:37 +0100

    added extension method for easily registering types.

---
+++ src/FluffySpoon.Extensions.MicrosoftDependencyInjection/RegistrationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace FluffySpoon.Extensions.MicrosoftDependencyInjection
+{
+    public static class RegistrationExtensions
+    {
+		public static void AddAssemblyTypesAsImplementedInterfaces(this ServiceCollection serviceCollection, params Assembly[] assemblies) {
+			foreach(var assembly in assemblies) {
+				var classTypes = assembly
+					.GetTypes()
+					.Where(x => x.IsClass);
+				foreach(var classType in classTypes) {
+					var implementedInterfaceTypes = classType.GetInterfaces();
+					foreach (var implementedInterfaceType in implementedInterfaceTypes)
+					{
+						serviceCollection.AddTransient(implementedInterfaceType, classType);
+					}
+				}
+			}
+		}
+    }
+}
