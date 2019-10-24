0b7314aff26ef1773c2878f33b5e5ef0f9893ac1
Mathias Lorenzen <mathias.lorenzen@broelstaerk.dk>
2019-10-24 10:58:29 +0200

    better namespacing.

--- src/FluffySpoon.Extensions.MicrosoftDependencyInjection/RegistrationFilters.cs
+++ src/FluffySpoon.Extensions.MicrosoftDependencyInjection/RegistrationFilters.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FluffySpoon.Extensions.MicrosoftDependencyInjection
 {
     public static class RegistrationFilters
     {
-        public static Func<Type, bool> TypesWithinNamespace(string @namespace)
+        public static Func<Type, bool> TypesWithinNamespace(params string[] namespaceFragments)
         {
+            var @namespace = GetNamespaceFromFragments(namespaceFragments);
             return t => t.Namespace?.StartsWith(@namespace) == true;
         }
 
-        public static Func<Type, bool> TypesWithNamespace(string @namespace)
+        public static Func<Type, bool> TypesWithNamespace(params string[] namespaceFragments)
         {
+            var @namespace = GetNamespaceFromFragments(namespaceFragments);
             return t => t.Namespace == @namespace;
         }
 
@@ -24,6 +27,13 @@ namespace FluffySpoon.Extensions.MicrosoftDependencyInjection
         public static Func<Type, bool> TypesWithinNamespaceOf(Type type)
         {
             return TypesWithinNamespace(type.Namespace);
+        }
+
+        private static string GetNamespaceFromFragments(string[] namespaceFragments)
+        {
+            return namespaceFragments
+                .DefaultIfEmpty()
+                .Aggregate((a, b) => a + "." + b);
         }
     }
 }
